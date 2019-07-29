119757124d1c6fe41464111369432b2ea4ec0dcf
unknown <raviv.vishwakarma@gmail.com>
2019-07-29 14:56:50 +0530

    modified delete function

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import UserTable from './UserTable'
 import Pagination from "./Pagination";
 
-
 export default class App extends React.Component {
     constructor(props) {
         super(props);
